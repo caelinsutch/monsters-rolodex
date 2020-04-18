95fb93c911ee2acc542e6aa9425c9d05ba577f2c
Caelin Sutch <caelinsutch@gmail.com>
2020-04-17 18:54:03 -0700

    Updates

---
+++ precache-manifest.117376f267daa5f2c282cbb0bf7f2aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9eb51eec72346dba9a639c00710040",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4540b677581b4662ad09",
+    "url": "/monsters-rolodex/static/css/main.6e08d333.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4540b677581b4662ad09",
+    "url": "/monsters-rolodex/static/js/main.b941845a.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
